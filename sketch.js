75d7fa127ff2c7b9e5aef427ea1789db2454ae01
Jeremiah Kendl <kendljeremiah@gmail.com>
2023-10-07 22:33:06 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,72 @@
 function setup() {
-  createCanvas(505, 505);
-  background(255); // Set the background to white
-  noLoop();
-  
-  let dotSize = 5;
-  let dotSpacing = 5;
-  
-  for (let x = 0; x < width; x += dotSpacing) {
-    for (let y = 0; y < height; y += dotSpacing) {
-      fill(0); // Set fill color to black
-      noStroke(); // No outline for dots
-      ellipse(x, y, dotSize, dotSize); // Draw a dot
+  createCanvas(1000, 500);
+  background(255);
+  textSize(32);
+  fill(0);
+
+  // Define the letters and their dot representations
+  const letters = {
+    'N': [
+      'X', '.', '.', '.', 'X',
+      'X', 'X', '.', '.', 'X',
+      '.', '.', '.', '.', '.',
+      'X', '.', '.', 'X', 'X',
+      'X', '.', '.', '.', 'X'
+    ],
+    'O': [
+      '.', '.', 'X', 'X', '.',
+      '.', 'X', '.', '.', 'X',
+      '.', '.', '.', '.', '.',
+      '.', 'X', '.', '.', 'X',
+      '.', '.', 'X', 'X', '.',
+    ],
+    'T': [
+      'X', 'X', 'X', 'X', 'X',
+      '.', '.', 'X', '.', '.',
+      '.', '.', '.', '.', '.',
+      '.', '.', 'X', '.', '.',
+      '.', '.', 'X', '.', '.',
+    ],
+    'X': [
+      'X', '.', '.', '.', 'X',
+      '.', 'X', '.', 'X', '.',
+      '.', '.', '.', '.', '.',
+      '.', 'X', '.', 'X', '.',
+      'X', '.', '.', '.', 'X',
+    ],
+    'I': [
+      '.', 'X', 'X', 'X', '.',
+      '.', '.', 'X', '.', '.',
+      '.', '.', '.', '.', '.',
+      '.', '.', 'X', '.', '.',
+      '.', 'X', 'X', 'X', '.',
+    ],
+    'C': [
+      '.', '.', 'X', 'X', 'X',
+      '.', 'X', '.', '.', '.',
+      '.', '.', '.', '.', '.',
+      '.', 'X', '.', '.', '.',
+      '.', '.', 'X', 'X', 'X',
+    ],
+  };
+
+  const word = "NONTOXIC";
+  const matrixSize = 5; // Size of each letter's matrix
+  const spacing = (120); // Spacing between letters
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word.charAt(i);
+    const letterMatrix = letters[letter];
+
+    for (let row = 0; row < matrixSize; row++) {
+      for (let col = 0; col < matrixSize; col++) {
+        const x = i * spacing + col * 20 + 50;
+        const y = row * 20 + 50;
+        const index = row * matrixSize + col;
+        if (letterMatrix[index] === 'X') {
+          ellipse(x, y, 5, 5);
+        }
+      }
     }
   }
-}
-
-function draw() {
-  textSize(120); // Adjust the font size as needed
-  fill(255); // Set the text color to match the background
-  textAlign(CENTER, CENTER);
-  
-  text("NON", width / 2, height / 2 - 50); // Adjust the vertical position as needed
-
-  // Position and draw "TOXIC" below "NON"
-  text("TOXIC", width / 2, height / 2 + 50); // Adjust the vertical position as needed
 }
